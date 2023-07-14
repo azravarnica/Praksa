d094d25047387d046d91c870c617b6516aab856a
Avarica1 <avarnica1@etf.unsa.ba>
2023-07-14 12:10:09 +0200

    Serijalizacija/Deserijalizacija

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +13,58 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
-namespace HelloWorld
+namespace PrviTask
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<string> myList;
         public MainWindow()
         {
             InitializeComponent();
+            myList = new List<string> { "Azra voli more", "Azra voli teretanu", "Azra voli planinarenje", "Azra ne voli da vozi automobil" };
+        }
+
+        private void serializeButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(myList, Formatting.Indented);
+                string filePath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "InformacijeAzrine.json");
+                File.WriteAllText(filePath, json);
+
+                MessageBox.Show("Serijalizacija uspješna!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greška prilikom serijalizacije: " + ex.Message);
+            }
+        }
+
+        private void deserializeButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string filePath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "InformacijeAzrine.json");
+                if (File.Exists(filePath))
+                {
+                    List<string> deserializedList = JsonConvert.DeserializeObject<List<string>>(File.ReadAllText(filePath));
+                    MessageBox.Show(string.Join(Environment.NewLine, deserializedList));
+                    //MessageBox.Show("Deserijalizacija uspješna!");
+                }
+                else
+                {
+                    MessageBox.Show("Fajl ne postoji.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greška prilikom deserijalizacije: " + ex.Message);
+            }
         }
     }
 }
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace HelloWorld.Properties
+namespace PrviTask.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace HelloWorld.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("HelloWorld.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PrviTask.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
