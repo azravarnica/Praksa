0699f727551f34c63dbf04ad5946c4e01877a739
Avarica1 <avarnica1@etf.unsa.ba>
2023-07-21 17:01:48 +0200

    Movie search app

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +13,97 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json.Linq;
 
-namespace HelloWorld
+namespace MovieSearchApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string apiKey = "64d8d178";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private int currentPage = 1;
+        private async void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            currentPage = 1;
+            string searchText = textSearch.Text;
+            if (string.IsNullOrEmpty(searchText))
+            {
+                MessageBox.Show("Enter a search term!");
+                return;
+
+            }
+            else if(searchText.Length > 8)
+            {
+                MessageBox.Show("Term is too long!");
+                return;
+            }
+            string apiUrl = $"https://www.omdbapi.com/?apikey={apiKey}&s={searchText}&page={currentPage}";
+            await SearchMovies(apiUrl);
+        }
+
+        private async void btnNextPage_Click(object sender, RoutedEventArgs e)
+        {
+            currentPage++;
+            string searchText = textSearch.Text;
+            if (string.IsNullOrEmpty(searchText))
+            {
+                MessageBox.Show("Enter a search term!");
+                return;
+
+            }
+            else if (searchText.Length > 8)
+            {
+                MessageBox.Show("Term is too long!");
+                return;
+            }
+            string apiUrl = $"https://www.omdbapi.com/?apikey={apiKey}&s={searchText}&page={currentPage}";
+            await SearchMovies(apiUrl);
+
+        }
+
+        private async Task SearchMovies(string apiUrl)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonResponse = await response.Content.ReadAsStringAsync();
+                        JObject jsonBody = JObject.Parse(jsonResponse);
+                        if (jsonBody["Response"].ToString().ToLower() == "true")
+                        {
+                            JArray movieArray = JArray.Parse(jsonBody["Search"].ToString());
+                            listMovies.Items.Clear();
+                            foreach (var item in movieArray)
+                            {
+                                listMovies.Items.Add($"Title: {item["Title"]}, Year: {item["Year"]}, imdbID: {item["imdbID"]}, Type: {item["Type"]}");
+                                btnNextPage.IsEnabled = (10 < int.Parse(jsonBody["totalResults"].ToString()));
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("No results found for this term");
+                        }
+                        
+                    }
+                    else
+                    {
+                        MessageBox.Show($"HTTP Error : {response.StatusCode}");
+                    }
+                } catch (HttpRequestException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
         }
     }
 }
